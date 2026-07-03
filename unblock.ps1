6e7a040511821d49b054cb9706854393411338f0
X <109253617+981726@users.noreply.github.com>
2026-07-03 00:17:04 -0700

    Revert driver auto-install and unlock settings
    Reverts changes to enable driver auto-install and unlock the Razer folder.

--- unblock.ps1
+++ unblock.ps1
@@ -1,1 +1,12 @@
+$RazerPath = "C:\Windows\Installer\Razer"
 
+# Enable driver auto-install via registry | Revert
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DriverSearching" -Name "SearchOrderConfig" -Type DWord -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Device Installer" -Name "DisableCoInstallers" -Type DWord -Value 0
+
+# Unlocks the Razer Folder | Revert
+$Acl = Get-Acl $RazerPath
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM", "Write", "ContainerInherit,ObjectInherit", "None", "Deny")
+
+$Acl.RemoveAccessRuleAll($Ar)
+Set-Acl $RazerPath $Acl
